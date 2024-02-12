c23951ec8aa258436e1c2125824a7604dcbb209a
Christian Le <crezle1999@gmail.com>
2024-02-12 13:49:53 +0100

    add A4 skeleton

---
+++ A4/python/common.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.patheffects as PathEffects
+import matplotlib.pyplot as plt
+
+#
+# Tip: Define functions to create the basic 4x4 transformations
+#
+# def translate_x(x): Translation along X-axis
+# def translate_y(y): Translation along Y-axis
+# def translate_z(z): Translation along Z-axis
+# def rotate_x(radians): Rotation about X-axis
+# def rotate_y(radians): Rotation about Y-axis
+# def rotate_z(radians): Rotation about Z-axis
+#
+# For example:
+def translate_x(x):
+    return np.array([[1, 0, 0, x],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+#
+# Note that you should use np.array, not np.matrix,
+# as the latter can cause some unintuitive behavior.
+#
+# translate_x/y/z could alternatively be combined into
+# a single function.
+
+def project(K, X):
+    """
+    Computes the pinhole projection of a 3xN array of 3D points X
+    using the camera intrinsic matrix K. Returns the dehomogenized
+    pixel coordinates as an array of size 2xN.
+    """
+
+    # Tip: Use the @ operator for matrix multiplication, the *
+    # operator on arrays performs element-wise multiplication!
+
+    #
+    # Placeholder code (replace with your implementation)
+    #
+    N = X.shape[1]
+    u = np.zeros([2,N])
+    return u
+
+def draw_frame(K, T, scale=1, labels=True):
+    """
+    Visualize the coordinate frame axes of the 4x4 object-to-camera
+    matrix T using the 3x3 intrinsic matrix K.
+
+    This uses your project function, so implement it first.
+
+    Control the length of the axes by specifying the scale argument.
+
+    Set labels=False to disable X, Y, Z labels.
+    """
+    X = T @ np.array([
+        [0,scale,0,0],
+        [0,0,scale,0],
+        [0,0,0,scale],
+        [1,1,1,1]])
+    u,v = project(K, X)  # If you get an error message here, you should modify your project function to accept 4xN arrays of homogeneous vectors, instead of 3xN.
+    plt.plot([u[0], u[1]], [v[0], v[1]], color='#cc4422') # X-axis
+    plt.plot([u[0], u[2]], [v[0], v[2]], color='#11ff33') # Y-axis
+    plt.plot([u[0], u[3]], [v[0], v[3]], color='#3366ff') # Z-axis
+    if labels:
+        textargs = {'color': 'w', 'va': 'center', 'ha': 'center', 'fontsize': 'x-small', 'path_effects': [PathEffects.withStroke(linewidth=1.5, foreground='k')]}
+        plt.text(u[1], v[1], 'X', **textargs)
+        plt.text(u[2], v[2], 'Y', **textargs)
+        plt.text(u[3], v[3], 'Z', **textargs)
---
+++ A4/python/task2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from common import *
+
+# Tip: Use np.loadtxt to load data into an array
+K = np.loadtxt('../data/task2K.txt')
+X = np.loadtxt('../data/task2points.txt')
+
+# Task 2.2: Implement the project function
+u,v = project(K, X)
+
+# You would change these to be the resolution of your image. Here we have
+# no image, so we arbitrarily choose a resolution.
+width,height = 600,400
+
+#
+# Figure for Task 2.2: Show pinhole projection of 3D points
+#
+plt.figure(figsize=(4,3))
+plt.scatter(u, v, c='black', marker='.', s=20)
+
+# The following commands are useful when the figure is meant to simulate
+# a camera image. Note: these must be called after all draw commands!
+
+plt.axis('image')     # This option ensures that pixels are square in the figure (preserves aspect ratio)
+                      # This must be called BEFORE setting xlim and ylim!
+plt.xlim([0, width])
+plt.ylim([height, 0]) # The reversed order flips the figure such that the y-axis points down
+plt.show()
