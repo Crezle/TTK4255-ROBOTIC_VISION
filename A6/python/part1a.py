18321b18c6010ef79e73319c31d0d361eea2649f
Christian Le <crezle1999@gmail.com>
2024-02-27 12:37:23 +0100

    complete task 1.1-1.6

--- A6/python/part1a.py
+++ A6/python/part1a.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from gauss_newton import gauss_newton
 from quanser import Quanser
+import os
 
-# Image to load data for (must be in [0, 350])
-image_number = 0
+if not os.path.exists('A6/plots'):
+    os.makedirs('A6/plots')
 
 # Tip:
 # "u" is a 2x7 array of detected marker locations.
@@ -12,10 +13,7 @@ image_number = 0
 # entries may be invalid if the corresponding markers were
 # not detected. Which entries are valid is encoded in
 # the "weights" array, which is a 1D array of length 7.
-detections = np.loadtxt('../data/detections.txt')
-weights = detections[image_number, ::3]
-u = np.vstack((detections[image_number, 1::3], detections[image_number, 2::3]))
-
+detections = np.loadtxt('A6/data/detections.txt')
 quanser = Quanser()
 
 # Tip:
@@ -27,24 +25,21 @@ quanser = Quanser()
 # must first define a "lambda function wrapper" that takes only a
 # single argument (the parameter vector), and likewise for computing
 # the Jacobian. This can be done like this:
+
+# Task 1.3 ("a")
+image_number = 0
+
+weights = detections[image_number, ::3]
+u = np.vstack((detections[image_number, 1::3], detections[image_number, 2::3]))
 resfun = lambda p : quanser.residuals(u, weights, p[0], p[1], p[2])
 
-# Tip:
-# These parameter values (yaw, pitch, roll) are close to the optimal
-# estimates for image 0.
-p = np.array([11.6, 28.9, 0.0])*np.pi/180
+p0 = np.array([11.6, 28.9, 0.0])*np.pi/180
 
-#
-# Task: Call gauss_newton
-#
+r = resfun(p0)
 
-r = resfun(p)
 print('Residuals on image %d:' % image_number)
 print(r)
 
-# Tip:
-# This snippet produces the requested outputs for the second half
-# of Task 1.3. The figure is saved to your working directory.
 reprojection_errors = np.linalg.norm(r.reshape((2,-1)), axis=0)
 print('Reprojection errors:')
 for i,reprojection_error in enumerate(reprojection_errors):
@@ -52,5 +47,65 @@ for i,reprojection_error in enumerate(reprojection_errors):
 print('Average:  %5.02f px' % np.mean(reprojection_errors))
 print('Median:   %5.02f px' % np.median(reprojection_errors))
 quanser.draw(u, weights, image_number)
-plt.savefig('out_part1a.png')
-plt.show()
+plt.savefig('A6/plots/out_part1a_task13b.png')
+if os.getenv("GITHUB_ACTIONS") != 'true':
+    plt.show()
+else:
+    plt.clf()
+
+
+# Task 1.3 ("b")
+image_number = 40
+
+weights = detections[image_number, ::3]
+u = np.vstack((detections[image_number, 1::3], detections[image_number, 2::3]))
+resfun = lambda p : quanser.residuals(u, weights, p[0], p[1], p[2])
+
+p = gauss_newton(resfun, p0, 0.9, 10)
+
+r = resfun(p)
+print('Residuals on image %d:' % image_number)
+print(r)
+
+reprojection_errors = np.linalg.norm(r.reshape((2,-1)), axis=0)
+reprojection_errors_text = 'Reprojection errors:\n'
+for i, reprojection_error in enumerate(reprojection_errors):
+    reprojection_errors_text += 'Marker %d: %5.02f px\n' % (i + 1, reprojection_error)
+reprojection_errors_text += 'Median:   %5.02f px' % np.median(reprojection_errors)
+
+quanser.draw(u, weights, image_number)
+plt.text(0.05, 0.05, reprojection_errors_text, transform=plt.gca().transAxes, va='bottom', ha='left', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
+plt.savefig('A6/plots/out_part1a_task13b.png')
+if os.getenv("GITHUB_ACTIONS") != 'true':
+    plt.show()
+else:
+    plt.clf()
+
+# Task 1.4
+
+# Test case 1: 
+#   p0 equal to first image position
+p0 = np.array([11.6, 28.9, 0.0])*np.pi/180
+p = gauss_newton(resfun, p0, 0.9, 10, xtol=0.01, print_progress=True)
+
+# Test case 2:
+#   p0 equal to an eye approx to position
+p0 = np.array([7.0, 5.0, 0.0])*np.pi/180
+p = gauss_newton(resfun, p0, 0.9, 10, xtol=0.01, print_progress=True)
+
+# Test case 3:
+#   p0 equal to large positional values
+p0 = np.array([180.0, 180.0, 180.0])*np.pi/180
+p = gauss_newton(resfun, p0, 0.9, 10, xtol=0.01, print_progress=True)
+
+
+# Task 1.5 (Commented out to prevent error)
+
+# image_number = 87
+
+# weights = detections[image_number, ::3]
+# u = np.vstack((detections[image_number, 1::3], detections[image_number, 2::3]))
+# resfun = lambda p : quanser.residuals(u, weights, p[0], p[1], p[2])
+# p0 = np.array([11.6, 28.9, 0.0])*np.pi/180
+
+# p = gauss_newton(resfun, p0, 0.9, 10, xtol=0.01, print_progress=True)
--- A6/python/quanser.py
+++ A6/python/quanser.py
@@ -4,11 +4,21 @@ from common import *
 
 class Quanser:
     def __init__(self):
-        self.K = np.loadtxt('../data/K.txt')
-        self.heli_points = np.loadtxt('../data/heli_points.txt').T
-        self.platform_to_camera = np.loadtxt('../data/platform_to_camera.txt')
+        self.K = np.loadtxt('A6/data/K.txt')
+        self.heli_points = np.loadtxt('A6/data/heli_points.txt').T
+        self.platform_to_camera = np.loadtxt('A6/data/platform_to_camera.txt')
 
     def residuals(self, u, weights, yaw, pitch, roll):
+        '''
+        Args:
+        u:          2xM array of detected marker locations
+        weights:    1D array of length M, where 1 indicates a valid detection
+        yaw:        Yaw angle in radians
+        pitch:      Pitch angle in radians
+        roll:       Roll angle in radians
+        
+        Returns:    1D array of length 2M, containing the residuals
+        '''
         # Compute the helicopter coordinate frames
         base_to_platform = translate(0.1145/2, 0.1145/2, 0.0)@rotate_z(yaw)
         hinge_to_base    = translate(0.00, 0.00,  0.325)@rotate_y(pitch)
@@ -22,20 +32,16 @@ class Quanser:
         # Compute the predicted image location of the markers
         p1 = self.arm_to_camera @ self.heli_points[:,:3]
         p2 = self.rotors_to_camera @ self.heli_points[:,3:]
+        # hat_u is a 2xM array of predicted marker locations.
         hat_u = project(self.K, np.hstack([p1, p2]))
         self.hat_u = hat_u # Save for use in draw()
 
-        #
-        # TASK: Compute the vector of residuals.
-        #
-        # Tip: Use np.hstack to concatenate the horizontal and vertical residual components
-        # into a single 1D array. Note: The plotting code will not work correctly if you use
-        # a different ordering.
-        r = np.zeros(2*7) # Placeholder, remove me!
+        r = ((hat_u - u)*weights).flatten()
+        
         return r
 
     def draw(self, u, weights, image_number):
-        I = plt.imread('../data/video%04d.jpg' % image_number)
+        I = plt.imread('A6/quanser_image_sequence/data/video%04d.jpg' % image_number)
         plt.imshow(I)
         plt.scatter(*u[:, weights == 1], linewidths=1, edgecolor='black', color='white', s=80, label='Observed')
         plt.scatter(*self.hat_u, color='red', label='Predicted', s=10)
