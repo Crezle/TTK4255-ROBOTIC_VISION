7e740ab3e58da9967bf994daaa48689029820cea
Christian Le <crezle1999@gmail.com>
2024-02-29 00:58:44 +0100

    complete task 2.1, A6

--- A6/python/part2.py
+++ A6/python/part2.py
@@ -2,16 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from common import *
 from scipy.optimize import least_squares
+import os
 
-K = np.loadtxt('../data/K.txt')
-u = np.loadtxt('../data/platform_corners_image.txt')
-X = np.loadtxt('../data/platform_corners_metric.txt')
-I = plt.imread('../data/video0000.jpg')
+K = np.loadtxt('A6/data/K.txt')
+u = np.loadtxt('A6/data/platform_corners_image.txt')
+X = np.loadtxt('A6/data/platform_corners_metric.txt')
+I = plt.imread('A6/quanser_image_sequence/data/video0000.jpg')
 
 # This is just an example. Replace these two lines
 # with your own code.
-hat_T = translate(0.0, 0.0, 1.0)
-hat_u = project(K, hat_T@X)
+
+def residuals(u, yaw, pitch, roll, z, y, x):
+    
+    rot_z = rotate_z(yaw)
+    rot_y = rotate_y(pitch)
+    rot_x = rotate_x(roll)
+    rot = rot_z @ rot_y @ rot_x
+    
+    hat_T = translate(x, y, z)
+    hat_u = project(K, hat_T@rot@X)
+    
+    r = (hat_u - u).flatten()
+    
+    return r
+
+resfun = lambda p: residuals(u, p[0], p[1], p[2], p[3], p[4], p[5])
+
+init_guess_roll = np.deg2rad(90)
+init_guess_pitch = np.deg2rad(0)
+init_guess_yaw = np.deg2rad(5)
+init_guess_z = 0.8
+init_guess_y = 0.0
+init_guess_x = 0.0
+p_init = np.array([init_guess_yaw, init_guess_pitch, init_guess_roll, init_guess_z, init_guess_y, init_guess_x])
+
+p_cam = least_squares(resfun, x0=p_init, method='lm').x
+
+hat_T = translate(p_cam[5], p_cam[4], p_cam[3])
+hat_R = rotate_z(p_cam[0]) @ rotate_y(p_cam[1]) @ rotate_x(p_cam[2])
+hat_u = project(K, hat_T @ hat_R @ X)
 
 reprojection_errors = np.linalg.norm(u - hat_u, axis=0)
 print('Reprojection errors:')
@@ -27,8 +56,11 @@ plt.legend()
 draw_frame(K, hat_T, scale=0.05, labels=True)
 
 # Tip: To zoom in on the platform:
-# plt.xlim([200, 500])
-# plt.ylim([600, 350])
+plt.xlim([200, 500])
+plt.ylim([600, 350])
 
-plt.savefig('out_part2.png')
-plt.show()
+plt.savefig('A6/plots/out_part2.png')
+if os.getenv("GITHUB_ACTIONS") != 'true':
+    plt.show()
+else:
+    plt.clf()
