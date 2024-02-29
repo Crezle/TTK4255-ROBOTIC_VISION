5d857b8b30f13a1152b721dd9441439818b602e5
Christian Le <crezle1999@gmail.com>
2024-02-29 01:27:55 +0100

    complete task 2.2, A6

--- A6/python/part2.py
+++ A6/python/part2.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from common import *
 from scipy.optimize import least_squares
 import os
+from enum import Enum
+
+class Task(Enum):
+    TASK2_1 = "task2_1"
+    TASK2_2_RUN1 = "task2_2_run1"
+    TASK2_2_RUN2 = "task2_2_run2"
 
 K = np.loadtxt('A6/data/K.txt')
 u = np.loadtxt('A6/data/platform_corners_image.txt')
@@ -26,7 +32,7 @@ def residuals(u, yaw, pitch, roll, z, y, x):
     
     return r
 
-resfun = lambda p: residuals(u, p[0], p[1], p[2], p[3], p[4], p[5])
+
 
 init_guess_roll = np.deg2rad(90)
 init_guess_pitch = np.deg2rad(0)
@@ -36,31 +42,43 @@ init_guess_y = 0.0
 init_guess_x = 0.0
 p_init = np.array([init_guess_yaw, init_guess_pitch, init_guess_roll, init_guess_z, init_guess_y, init_guess_x])
 
-p_cam = least_squares(resfun, x0=p_init, method='lm').x
+for task in Task:
+    if task == Task.TASK2_2_RUN1:
+        u = u[:, 1:]
+        X = X[:, 1:]
+    
+    if task == Task.TASK2_2_RUN2:
+        p_init = np.array([1, 1, 1, 5, 0.0, 0.0])
 
-hat_T = translate(p_cam[5], p_cam[4], p_cam[3])
-hat_R = rotate_z(p_cam[0]) @ rotate_y(p_cam[1]) @ rotate_x(p_cam[2])
-hat_u = project(K, hat_T @ hat_R @ X)
+    resfun = lambda p: residuals(u, p[0], p[1], p[2], p[3], p[4], p[5])
+    p_cam = least_squares(resfun, x0=p_init, method='lm').x
 
-reprojection_errors = np.linalg.norm(u - hat_u, axis=0)
-print('Reprojection errors:')
-for e in reprojection_errors:
-    print('%.05f px' % e)
+    hat_R = rotate_z(p_cam[0]) @ rotate_y(p_cam[1]) @ rotate_x(p_cam[2])
+    hat_T = translate(p_cam[5], p_cam[4], p_cam[3]) @ hat_R
+    
+    hat_u = project(K, hat_T @ X)
+    if task != Task.TASK2_1:
+        np.savetxt(f'A6/plots/out_part2_{task}_Tmtrx.txt', hat_T)
 
-plt.imshow(I)
-plt.scatter(u[0,:], u[1,:], marker='o', facecolors='white', edgecolors='black', label='Detected')
-plt.scatter(hat_u[0,:], hat_u[1,:], marker='.', color='red', label='Predicted')
-plt.legend()
+    reprojection_errors = np.linalg.norm(u - hat_u, axis=0)
+    print('Reprojection errors:')
+    for e in reprojection_errors:
+        print('%.05f px' % e)
 
-# Tip: Draw the axes of a coordinate frame
-draw_frame(K, hat_T, scale=0.05, labels=True)
+    plt.imshow(I)
+    plt.scatter(u[0,:], u[1,:], marker='o', facecolors='white', edgecolors='black', label='Detected')
+    plt.scatter(hat_u[0,:], hat_u[1,:], marker='.', color='red', label='Predicted')
+    plt.legend()
 
-# Tip: To zoom in on the platform:
-plt.xlim([200, 500])
-plt.ylim([600, 350])
+    # Tip: Draw the axes of a coordinate frame
+    draw_frame(K, hat_T, scale=0.05, labels=True)
 
-plt.savefig('A6/plots/out_part2.png')
-if os.getenv("GITHUB_ACTIONS") != 'true':
-    plt.show()
-else:
-    plt.clf()
+    # Tip: To zoom in on the platform:
+    plt.xlim([200, 500])
+    plt.ylim([600, 350])
+
+    plt.savefig(f'A6/plots/out_part2_{task}.png')
+    if os.getenv("GITHUB_ACTIONS") != 'true':
+        plt.show()
+    else:
+        plt.clf()
